51a6ffc7c1740ed5a9d87b5ffb1462de2dbb9dc9
leedo <leedoyun98@naver.com>
2021-02-08 14:19:25 +0900

    ldy

--- ncsproject/src/main/java/com/example/demo/HomeController.java
+++ ncsproject/src/main/java/com/example/demo/HomeController.java
@@ -1,39 +1,40 @@
 package com.example.demo;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;   
+import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
+@SessionAttributes({"demo","board","login"})
 public class HomeController {
+	@Autowired HttpSession session;
+	@Autowired HttpServletRequest request;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	 @GetMapping("/")
+	    public String index(HttpSession session, HttpServletRequest request) {
+	    	String demo = request.getContextPath();
+	    	session.setAttribute("demo", demo);
+	    	session.setAttribute("board", demo+"/resources/board");
+	    	session.setAttribute("login", demo+"/resources/login");
+	    	 System.out.println("메인");
+	        return "main";
+	    }
+	@GetMapping("/admin/{page}")
+    public String admin(@PathVariable String page) {
+		 System.out.println("이동: " +page);
+        return String.format("admin:%s",page);
+    }
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	@GetMapping("/login/{page}")
+    public String login(@PathVariable String page) {
+		 System.out.println("로그인컨트: " +page);
+        return String.format("login:%s",page);
+    }
+	
 	
 }
