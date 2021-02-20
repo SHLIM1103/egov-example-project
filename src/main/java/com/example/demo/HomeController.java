b26a2ed36381ca13262a617e7900c8fb97a6f13f
jjwjun2 <jj9w1@naver.com>
2021-02-21 02:49:32 +0900

    edit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes({"c", "adm"})
+@SessionAttributes({ "c", "adm" })
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
@@ -26,27 +27,35 @@ public class HomeController {
 	public String home(HttpSession session, HttpServletRequest request) {
 		String c = request.getContextPath();
 		session.setAttribute("c", c);
-		// js를 쓸 수 있도록 head-home으로 가는 경로를 지정해주는 
-		session.setAttribute("adm", c+"/resources/adm");
+		// js를 쓸 수 있도록 head-home으로 가는 경로를 지정해주는
+		session.setAttribute("adm", c + "/resources/adm");
 		System.out.println("Check c path: " + c);
 		return "home";
 	}
 
-	@GetMapping("/manage/{page}")
-	public String move(@PathVariable String page) {
-		System.out.println("Check c path: " + page);
-		return String.format("manage:%s", page);
-	}
-
 	@GetMapping("/manage/{page1}/{page2}")
 	public String transfer(@PathVariable String page1, @PathVariable String page2) {
-		System.out.println("Check c path: " + page1 + "/ " + page2);
+		System.out.println("Check 2개  path: " + page1 + "/ " + page2);
 		return String.format("manage:%s/%s", page1, page2);
 	}
-
-	@GetMapping("/manage/{page1}/{page2}/{page3}")
-	public String transfer(@PathVariable String page1, @PathVariable String page2, @PathVariable String page3) {
-		System.out.println("Check c path: " + page1 + "/" + page2 + "/" + page3);
-		return String.format("manage:%s/%s/%s", page1, page2, page3);
+	
+	@GetMapping("/manage/{page1}/{page2}/{id}")
+	public String transfers(@PathVariable String page1, @PathVariable String page2, @PathVariable String id, Model model) {
+		var map = new HashMap();
+		System.out.println("Detail c path: " + page1 + "/ " + page2 + "/" + id);
+//		map.put("message", memberMapper.?:);
+		// key값으로 전달
+		model.addAttribute("id", id);
+		return String.format("manage2:%s/%s", page1, page2);
+	}
+	
+	
+	@GetMapping("/detail/{page1}/{id}")
+	public String transferss(@PathVariable String page1, @PathVariable String id, Model model) {
+		System.out.println("Check c path: " + page1 + "/"+ id);
+		// key값으로 전달
+		model.addAttribute("id", id);
+		System.out.println(model.addAttribute("id", id));
+		return String.format("manage3:%s/%s", page1, id);
 	}
 }
